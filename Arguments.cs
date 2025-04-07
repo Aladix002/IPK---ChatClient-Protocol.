06329f15d1ab2b0a0072b248f674b7718a2bc7fd
Aladix002 <xbotlo01@stud.fit.vut.cz>
2025-04-07 13:46:55 +0200

    spracovanie argumentov

---
+++ Arguments.cs
@@ -0,0 +1,19 @@
+using CommandLine;
+
+public record Arguments
+{
+    [Option('t', Required = true, HelpText = "Transport protocol (tcp or udp)")]
+    public string Protocol { get; init; } = "";
+
+    [Option('s', Required = true, HelpText = "Server IP or hostname")]
+    public string Ip { get; init; } = "";
+
+    [Option('p', Default = 4567, HelpText = "Server port")]
+    public int Port { get; init; }
+
+    [Option('d', Default = 250, HelpText = "UDP confirm timeout (ms)")]
+    public int UdpTimeout { get; init; }
+
+    [Option('r', Default = 3, HelpText = "UDP max retries")]
+    public int MaxRetries { get; init; }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+return Parser.Default
+    .ParseArguments<Arguments>(args)
+    .MapResult(
+        RunClient,
+        errs => 1
+    );
+
+static int RunClient(Arguments opts)
+{
+    Console.WriteLine($"Protocol: {opts.Protocol}");
+    Console.WriteLine($"Server: {opts.Ip}:{opts.Port}");
+    Console.WriteLine($"UDP timeout: {opts.UdpTimeout}ms, Retries: {opts.MaxRetries}");
+
+
+    return 0;
+}
+
